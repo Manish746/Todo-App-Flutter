6c69d4d6abbd5561abe9401b86f97e2b14d19c8c
Manish746 <mjaiswal128@gmail.com>
2020-12-16 13:44:55 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_hive/homepage.dart';
+import 'package:todo_hive/model/todo_model.dart';
+
+const String todoBoxName = "todo";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final document = await getApplicationDocumentsDirectory();
+  Hive.init(document.path);
+  Hive.registerAdapter(TodoModelAdapter());
+  await Hive.openBox<TodoModel>(todoBoxName);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
